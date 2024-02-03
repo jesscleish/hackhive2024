44956f4b76048c405759517f1c40f2db19476350
Jessica Leishman <71527196+jesscleish@users.noreply.github.com>
2024-02-03 15:12:50 -0500

    more templating

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -7,12 +7,19 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
 #    return 'Hello World!'
 
-#this will be our login page
-    return '<a href="/admin/home.html">admin stuff</a> <br> <a href="/employee/home.html">employee stuff</a>'
+#this will be our login page eventually
+    return '<a href="/admin">admin stuff</a> <br> <a href="/employee/">employee stuff</a>'
+#
 
 @app.route('/admin')
 def admin_home():
     render_template('admin/home.html') #this should work properly??
+    #templates/admin/home ?
+
+@app.route('/employee')
+def employee_home():
+    render_template('employee/home.html')
+    #templates/employee/home ?
 
 if __name__ == '__main__':
     app.run()
