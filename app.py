4cfd535abeeabe4c2314d24e12ac14dc63fc5bf0
Jessica Leishman <71527196+jesscleish@users.noreply.github.com>
2024-02-03 12:45:12 -0500

    html templates

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+#    return 'Hello World!'
 
+#this will be our login page
+    return '<a href="/admin/home.html">admin stuff</a> <br> <a href="/employee/home.html">employee stuff</a>'
+
+@app.route('/admin')
+def admin_home():
+    render_template('admin/home.html') #this should work properly??
 
 if __name__ == '__main__':
     app.run()
